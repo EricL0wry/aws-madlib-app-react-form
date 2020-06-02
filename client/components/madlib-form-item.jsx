bda6dc9d16e0dedf1663f9e51c588296aec61381
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 21:23:37 -0700

    Set up form to dynamically render form items. Created handleChange method to update state with each input change

--- client/components/madlib-form-item.jsx
+++ client/components/madlib-form-item.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 export default function MadlibFormItem(props) {
-
+  const { id, label, handleChange } = props;
   return (
-    <div>form item</div>
+    <div className="form-group col-6">
+      <label htmlFor={id}>{label}</label>
+      <input type="text" className="form-control" id={id} onChange={handleChange}/>
+    </div>
   );
 }
--- client/components/madlib-form.jsx
+++ client/components/madlib-form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MadlibFormItem from './madlib-form-item';
 
 export default class MadlibForm extends React.Component {
   constructor(props) {
@@ -6,6 +7,7 @@ export default class MadlibForm extends React.Component {
     this.state = {
       madlib: null
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -16,15 +18,49 @@ export default class MadlibForm extends React.Component {
     fetch(`/api/madlib/${this.props.params.id}`)
       .then(res => res.json())
       .then(data => {
-        this.setState({ madlib: data });
+        const formData = {};
+        for (const key in data.words) {
+          formData[key] = '';
+        }
+        formData.userName = '';
+        formData.madLibId = this.props.params.id;
+        this.setState({
+          madlib: data,
+          returnData: formData
+        });
       })
       .catch(err => console.error(err));
   }
 
+  handleChange(event) {
+    const returnData = Object.assign({}, this.state.returnData);
+    const { id, value } = event.target;
+    returnData[id] = value;
+    this.setState({ returnData });
+  }
+
   render() {
+    const formItems = [];
+    if (this.state.madlib) {
+      for (const key in this.state.madlib.words) {
+        const formItem = <MadlibFormItem
+          key={key}
+          id={key}
+          label={this.state.madlib.words[key]}
+          handleChange={this.handleChange}
+        />;
+        formItems.push(formItem);
+      }
+    }
     return (
       <div className="container">
-        {this.props.params.id}
+        <form className="row">
+          <div className="form-group col-12">
+            <label htmlFor="userName">Your Name</label>
+            <input type="text" className="form-control" id="userName" onChange={this.handleChange}/>
+          </div>
+          {formItems}
+        </form>
       </div>
     );
 
