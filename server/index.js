3159d854c1743e62b8c9709a0c73c1e352cdf2be
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 17:29:35 -0700

    Required node-fetch package

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 require('dotenv/config');
 const express = require('express');
+const fetch = require('node-fetch');
 
 const staticMiddleware = require('./static-middleware');
-
 const app = express();
 
 app.use(staticMiddleware);
 
 app.use(express.json());
+
+app.get'/api/madlibs', (req, res) => {
+
+})
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
