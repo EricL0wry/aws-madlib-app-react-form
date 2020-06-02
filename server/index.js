ba438ea33381d7709eb48bfbf451695465a90f64
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 17:47:09 -0700

    Added endpoint to proxy madlib list request

--- server/index.js
+++ server/index.js
@@ -9,9 +9,15 @@ app.use(staticMiddleware);
 
 app.use(express.json());
 
-app.get'/api/madlibs', (req, res) => {
-
-})
+app.get('/api/madlibs', (req, res) => {
+  fetch('https://rq44dndifg.execute-api.us-east-1.amazonaws.com/dev/get-madlib-list')
+    .then(response => response.json())
+    .then(madlibs => res.json(madlibs))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred' });
+    });
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
