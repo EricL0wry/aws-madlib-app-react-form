e87c716e4484018d48c364d5c3345e3fa84e841f
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 17:28:28 -0700

    Updated app component to render madlib list component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import MadlibList from './madlib-list';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: { name: 'madlibList', params: {} }
+    };
+  }
 
   render() {
+    const { name, params } = this.state.view;
+    const list = <MadlibList />;
+    let view;
+    if (name === 'madlibList') view = list;
     return (
-      <h1>React App</h1>
+      <div>
+        {view}
+      </div>
     );
   }
 }
