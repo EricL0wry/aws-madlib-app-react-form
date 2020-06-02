da831562811f2710c42a904acb99977d45e072df
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 19:04:15 -0700

    Created madlib list item component and did initial work to display title and select button

---
+++ client/components/madlib-list-item.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function MadlibListItem(props) {
+  const { madLibId, madLibName } = props.madlib;
+  return (
+    <div className="col-12">
+      <h4>{madLibName}</h4>
+      <button
+        type="button"
+        className="btn select-button"
+        onClick={() => props.setView('form', { madLibId })}
+      >
+        Select
+      </button>
+    </div>
+  );
+}
