3c844b086771da8f10e903178c3ad8196fa5f2b8
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 19:02:03 -0700

    Added setView method to be passed as props

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MadlibList from './madlib-list';
+import Header from './header';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -7,16 +8,26 @@ export default class App extends React.Component {
     this.state = {
       view: { name: 'madlibList', params: {} }
     };
+    this.setView = this.setView.bind(this);
+  }
+
+  setView(name, params) {
+    this.setState({
+      view: { name, params }
+    });
   }
 
   render() {
     const { name, params } = this.state.view;
-    const list = <MadlibList />;
+    const list = <MadlibList setState={this.setView}/>;
     let view;
     if (name === 'madlibList') view = list;
     return (
-      <div>
-        {view}
+      <div className="wrapper">
+        <Header />
+        <div className="container">
+          {view}
+        </div>
       </div>
     );
   }
