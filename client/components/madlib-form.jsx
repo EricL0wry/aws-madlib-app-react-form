6aebf7b626aeffda5031b7410b6b2020cbf0bf31
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 22:07:18 -0700

    Completed form functionality, form validation, and handled submit to change view

--- client/components/madlib-form.jsx
+++ client/components/madlib-form.jsx
@@ -5,9 +5,11 @@ export default class MadlibForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      madlib: null
+      madlib: null,
+      isComplete: null
     };
     this.handleChange = this.handleChange.bind(this);
+    this.validateInputs = this.validateInputs.bind(this);
   }
 
   componentDidMount() {
@@ -26,17 +28,32 @@ export default class MadlibForm extends React.Component {
         formData.madLibId = this.props.params.id;
         this.setState({
           madlib: data,
-          returnData: formData
+          formData: formData
         });
       })
       .catch(err => console.error(err));
   }
 
   handleChange(event) {
-    const returnData = Object.assign({}, this.state.returnData);
+    const formData = Object.assign({}, this.state.formData);
     const { id, value } = event.target;
-    returnData[id] = value;
-    this.setState({ returnData });
+    formData[id] = value;
+    this.setState({ formData });
+  }
+
+  validateInputs() {
+    const { formData } = this.state;
+    let formComplete = true;
+    for (const key in formData) {
+      if (formData[key].length < 1) {
+        formComplete = false;
+      }
+    }
+    if (!formComplete) {
+      this.setState({ isComplete: false });
+    } else {
+      this.props.setView('madlibSubmit', this.state.formData);
+    }
   }
 
   render() {
@@ -61,6 +78,15 @@ export default class MadlibForm extends React.Component {
           </div>
           {formItems}
         </form>
+        <div className="row">
+          <button
+            type="button"
+            className="btn submit-button"
+            onClick={this.validateInputs}
+          >
+            Submit!
+          </button>
+        </div>
       </div>
     );
 
